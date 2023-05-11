f0dbee3564556257bf2263c0def72021c16fd6c8
stavroskiig <stavroskiig@gmail.com>
2023-05-11 16:17:47 +0300

    Beautify the code

--- directBispectrum.m
+++ directBispectrum.m
@@ -1,60 +1,81 @@
-function Bspec = directBispectrum (X, nfft, wind, nsamp, overlap)
+function Bspec = directBispectrum (X, fftLength, windowType, sampleSize, percentOverlap)
 %% Parameter Validation
-[ly, nrecs] = size(X);
-if (ly == 1) 
+
+% Get the number of rows and columns of the input data matrix X
+[numRows, numCols] = size(X);
+
+% If X has only one row, reshape it into a column vector
+if (numRows == 1) 
     X = X(:);  
-    ly = nrecs; 
-    nrecs = 1; 
-end
-if (exist('nfft','var') ~= 1)            
-    nfft = 128; 
-end
-if (exist('overlap','var') ~= 1)      
-    overlap = 50;  
-end
-overlap = min(99,max(overlap,0));
-if (nrecs > 1)                  
-    overlap =  0;  
-end
-if (exist('nsamp','var') ~= 1)          
-    nsamp = 0;   
-end
-if (nrecs > 1)                    
-    nsamp = ly;  
-end
-if (nrecs == 1 && nsamp <= 0)
-nsamp = fix(ly/ (8 - 7 * overlap/100));
-end
-if (nfft  < nsamp)   
-    nfft = 2^nextpow2(nsamp); 
+    numRows = numCols; 
+    numCols = 1; 
 end
 
-overlap  = fix(nsamp * overlap / 100);
-nadvance = nsamp - overlap;
-nrecs    = fix ( (ly*nrecs - overlap) / nadvance);
+% Check if the variable 'fftLength' is not defined, set it to the default value 128
+if (exist('fftLength','var') ~= 1)            
+    fftLength = 128; 
+end
+
+% Check if the variable 'percentOverlap' is not defined, set it to the default value 50
+if (exist('percentOverlap','var') ~= 1)      
+    percentOverlap = 50;  
+end
+
+% Ensure that percentOverlap is within the range of 0 to 99
+percentOverlap = min(99, max(percentOverlap, 0));
+
+% If there are multiple columns in X, set percentOverlap to 0
+if (numCols > 1)                  
+    percentOverlap =  0;  
+end
+
+% Check if the variable 'sampleSize' is not defined, set it to 0
+if (exist('sampleSize','var') ~= 1)          
+    sampleSize = 0;   
+end
+
+% If there are multiple columns in X, set sampleSize to the number of rows
+if (numCols > 1)                    
+    sampleSize = numRows;  
+end
+
+% If X has only one column and sampleSize is not positive, compute sampleSize based on percentOverlap
+if (numCols == 1 && sampleSize <= 0)
+sampleSize = fix(numRows/ (8 - 7 * percentOverlap/100));
+end
+
+% If fftLength is smaller than sampleSize, set it to the smallest power of 2 that is greater than or equal to sampleSize
+if (fftLength  < sampleSize)   
+    fftLength = 2^nextpow2(sampleSize); 
+end
+
+% Compute the number of columns based on numRows, numCols, percentOverlap, and nadvance
+percentOverlap = fix(sampleSize * percentOverlap / 100);
+nAdvance = sampleSize - percentOverlap;
+numCols = fix( (numRows*numCols - percentOverlap) / nAdvance);
 
 
 %% 2D Window Creation
-if (exist('wind', 'var') ~= 1) 
-    wind = 5; 
+if (exist('windowType', 'var') ~= 1) 
+    windowType = 5; 
 end
-[m,n] = size(wind);
-window = wind;
+[m,n] = size(windowType);
+window = windowType;
 
 if (max(m,n) == 1)     % scalar: wind is size of Rao-Gabr window
-    winsize = wind;
+    winsize = windowType;
     if (winsize < 0) 
         winsize = 5; 
     end        % the window length L
     winsize = winsize - rem(winsize,2) + 1;  % make it odd
     if (winsize > 1)
-        mwind   = fix (nfft/winsize);            % the scale parameter M
+        mwind   = fix (fftLength/winsize);            % the scale parameter M
         lby2    = (winsize - 1)/2;
 
         theta  = -lby2:lby2;
         opwind = ones(winsize,1) * (theta .^2);       % w(m,n)=m^2
         opwind = opwind + opwind' + theta' * theta;   % m^2 + n^2 + mn
-        opwind = 1 - (2*mwind/nfft)^2 * opwind;       %
+        opwind = 1 - (2*mwind/fftLength)^2 * opwind;       %
         hex    = ones(winsize,1) * theta;             % m
         hex    = abs(hex) + abs(hex') + abs(hex+hex');
         hex    = (hex < winsize);
@@ -96,33 +117,33 @@ else                    % 2-D window passed: use directly
 end
 
 %% Accumulate Triple Products
-Bspec    = zeros(nfft,nfft);
+Bspec    = zeros(fftLength,fftLength);
 
-mask = hankel(1:nfft,[nfft,1:nfft-1] );   % the hankel mask (faster)
-locseg = (1:nsamp)';
-for krec = 1:nrecs
+mask = hankel(1:fftLength,[fftLength,1:fftLength-1] );   % the hankel mask (faster)
+locseg = (1:sampleSize)';
+for krec = 1:numCols
     xseg   = X(locseg);
-    Xf     = fft(xseg-mean(xseg), nfft)/nsamp;
+    Xf     = fft(xseg-mean(xseg), fftLength)/sampleSize;
     CXf    = conj(Xf);
-    Bspec  = Bspec + (Xf * Xf.') .*reshape(CXf(mask), nfft, nfft);
-    locseg = locseg + nadvance;
+    Bspec  = Bspec + (Xf * Xf.') .*reshape(CXf(mask), fftLength, fftLength);
+    locseg = locseg + nAdvance;
 end
 
-Bspec = fftshift(Bspec)/(nrecs);
+Bspec = fftshift(Bspec)/(numCols);
 
 %% Frequency-Domain Smoothing
 if (winsize > 1)
     lby2 = (winsize-1)/2;
     Bspec = conv2(Bspec,opwind);
-    Bspec = Bspec(lby2+1:lby2+nfft,lby2+1:lby2+nfft);
+    Bspec = Bspec(lby2+1:lby2+fftLength,lby2+1:lby2+fftLength);
 end
 
 
 %% Contour Plot of Magnitude Bispectum
-if (rem(nfft,2) == 0)
-    waxis = (-nfft/2:(nfft/2-1))'/nfft;
+if (rem(fftLength,2) == 0)
+    waxis = (-fftLength/2:(fftLength/2-1))'/fftLength;
 else
-    waxis = (-(nfft-1)/2:(nfft-1)/2)'/nfft;
+    waxis = (-(fftLength-1)/2:(fftLength-1)/2)'/fftLength;
 end
 
 contour(waxis,waxis,abs(Bspec),4),grid on 
--- indirectBispectrum.m
+++ indirectBispectrum.m
@@ -1,112 +1,213 @@
-function Bspec = indirectBispectrum (X, nlag, nsamp,overlap, flag, nfft, wind)
+function bispectrum = indirectBispectrum (X, maxLag, sampleSize,percentOverlap, biasCorrection, fftLength, windowType)
 %% Parameter Validation
-[ly, nrecs] = size (X); 
-if (ly == 1) 
-    X=X(:);   
-    ly = nrecs; 
-    nrecs = 1;      
+
+% Get the number of rows and columns of the input data matrix X
+[numRows, numCols] = size(X); 
+
+% If X has only one row, reshape it into a column vector
+if (numRows == 1) 
+    X = X(:);   
+    numRows = numCols; 
+    numCols = 1;      
 end 
-if (exist('overlap','var') ~= 1)   
-    overlap = 0;          
-end 
-overlap = min(99, max(overlap,0)); 
-if (nrecs > 1)               
-    overlap = 0;          
-end 
-if (exist('nsamp','var') ~= 1)     
-    nsamp   = ly;         
+
+% Check if the variable 'percentOverlap' is not defined, set it to the default value 50
+if (exist('percentOverlap','var') ~= 1)   
+    percentOverlap = 0;          
 end
-if (nsamp > ly || nsamp <= 0) 
-    nsamp   = ly;         
-end
-if (exist('flag','var') ~= 1)      
-    flag    = 'biased';   
+
+% Ensure that percentOverlap is within the range of 0 to 99
+percentOverlap = min(99, max(percentOverlap, 0)); 
+
+% If there are multiple columns in X, set percentOverlap to 0
+if (numCols > 1)               
+    percentOverlap = 0;          
 end 
-if (flag(1:1) ~= 'b')        
-    flag    = 'unbiased'; 
-end 
-if (exist('nfft','var') ~= 1)      
-    nfft    = 128;        
+
+% Check if the variable 'sampleSize' is not defined, set it to numRows
+if (exist('sampleSize', 'var') ~= 1)     
+    sampleSize = numRows;         
 end
-if (nfft <= 0)               
-    nfft    = 128;        
-end 
-if (exist('wind','var') ~= 1)      
-    wind    = 0;          
+
+% Ensure that sampleSize is within the valid range
+if (sampleSize > numRows || sampleSize <= 0) 
+    sampleSize = numRows;         
 end
-nlag = min(nlag, nsamp-1); 
-if (nfft  < 2*nlag+1)   
-    nfft = 2^nextpow2(nsamp); 
+
+% Check if the variable 'biasCorrection' is not defined, set it to the default value 'biased'
+if (exist('biasCorrection', 'var') ~= 1)      
+    biasCorrection = 'biased';   
+end
+
+% If biasCorrection doesn't start with 'b', set it to 'unbiased'
+if (biasCorrection(1:1) ~= 'b')        
+    biasCorrection = 'unbiased'; 
+end 
+
+% Check if the variable 'fftLength' is not defined, set it to the default value 128
+if (exist('fftLength', 'var') ~= 1)      
+    fftLength = 128;        
+end
+
+% Ensure that fftLength is a positive value
+if (fftLength <= 0)               
+    fftLength = 128;        
+end 
+
+% Check if the variable 'windowType' is not defined, set it to 0
+if (exist('windowType', 'var') ~= 1)      
+    windowType = 0;          
+end
+
+% Limit the maximum lag value to be within the range of 0 to sampleSize-1
+maxLag = min(maxLag, sampleSize-1); 
+
+% If fftLength is smaller than 2*maxLag+1, set it to the smallest power of 2 that is greater than or equal to sampleSize
+if (fftLength < 2*maxLag + 1)   
+    fftLength = 2^nextpow2(sampleSize); 
 end  
 
 %% Window Creation
-if (wind == 0) 
-    window = [0; parzenwin(nlag)];
+
+% Check if the windowType is 0
+if (windowType == 0)
+    % If windowType is 0, create a window using Parzen window function with a length of maxLag
+    window = [0; parzenwin(maxLag)];
 else
-     window = ones(nlag+1,1);     
-end 
-window = [window; zeros(nlag,1)];
+     % If windowType is not 0, create a rectangular window with a length of maxLag+1
+     window = ones(maxLag+1, 1);     
+end
+
+% Append zeros to the window to make its length equal to 2*maxLag+1
+window = [window; zeros(maxLag, 1)];
 
 %% Cumulants in non-redundant region
-overlap  = fix(nsamp * overlap / 100); 
-nadvance = nsamp - overlap; 
-nrecord  = fix ( (ly*nrecs - overlap) / nadvance ); 
 
-c3 = zeros(nlag+1,nlag+1);
-ind = (1:nsamp)';
-for k=1:nrecord
-    x = X(ind); x = x - mean(x);
-    ind = ind + nadvance; 
-    for j=0:nlag
-        z = x(1:nsamp-j) .* x(j+1:nsamp); 
-        for i=j:nlag
-            sum = z(1:nsamp-i)' * x(i+1:nsamp); 
-            if (flag(1:1) == 'b'), sum = sum/nsamp; 
-            else, sum = sum / (nsamp-i); 
-            end 
-            c3(i+1,j+1) = c3(i+1,j+1) + sum; 
+% Compute the actual overlap value based on the sample size and the given percentOverlap
+actualOverlap = fix(sampleSize * percentOverlap / 100);
+
+% Compute the advance between successive sample windows
+windowAdvance  = sampleSize - actualOverlap;
+
+% Compute the number of non-redundant records based on the total number of rows, columns, and the advance
+numNonRedundantRecords  = fix((numRows * numCols - actualOverlap) / windowAdvance );
+
+% Initialize the cumulants matrix
+cumulantsMatrix = zeros(maxLag+1, maxLag+1);
+
+% Create an index vector for selecting the samples in each window
+windowIndices = (1:sampleSize)';
+
+% Loop over each record
+for recordIndex = 1:numNonRedundantRecords 
+    % Extract the samples for the current window
+    windowSamples = X(windowIndices);
+    
+    % Subtract the mean from the samples
+    windowSamples = windowSamples - mean(windowSamples);
+    
+    % Update the index for the next window
+    windowIndices = windowIndices + nadvance;
+    
+    % Loop over the lag values
+    for lag = 0:maxLag
+        % Compute the product of samples with lag
+        laggedProducts = windowSamples(1:sampleSize-lag) .* windowSamples(lag+1:sampleSize);
+        
+        % Loop over the cumulant order values
+        for order = lag:maxLag
+            % Compute the sum of products for the current lag and order
+            productSum = laggedProducts(1:sampleSize-order)' * windowSamples(order+1:sampleSize);
+            
+            % Apply bias correction if needed
+            if (biasCorrection(1:1) == 'b')
+                productSum = productSum / sampleSize;
+            else
+                productSum = productSum / (sampleSize - order);
+            end
+            
+            % Update the cumulants matrix
+            cumulantsMatrix(order+1, lag+1) = cumulantsMatrix(order+1, lag+1) + productSum;
         end
     end
 end
-c3 = c3 / nrecord; 
+
+% Normalize the cumulants matrix by the number of records
+cumulantsMatrix = cumulantsMatrix / numNonRedundantRecords ;
 
 %% Cumulants elsewhere by symmetry
-c3 = c3 + tril(c3,-1)';
-c31 = c3(2:nlag+1,2:nlag+1); 
-c32 = zeros(nlag,nlag);  c33 = c32;  c34 = c32; 
-for i=1:nlag 
-    x = c31(i:nlag,i); 
-    c32(nlag+1-i,1:nlag+1-i) = x'; 
-    c34(1:nlag+1-i,nlag+1-i) = x; 
-    if (i < nlag) 
-        x = flipud(x(2:length(x))); 
-        c33 = c33 + diag(x,i) + diag(x,-i); 
+
+% Apply symmetry to the cumulants matrix
+symmetricCumulantsMatrix = cumulantsMatrix + tril(cumulantsMatrix, -1)';
+
+% Extract the submatrix excluding the first row and column
+cumulantsElsewhere = symmetricCumulantsMatrix(2:maxLag+1, 2:maxLag+1); 
+
+% Initialize matrices for different cumulant components
+cumulantComponent32 = zeros(maxLag, maxLag);  
+cumulantComponent33 = cumulantComponent32;  
+cumulantComponent34 = cumulantComponent32; 
+
+% Loop over each order
+for order = 1:maxLag
+    % Extract the window samples for the current order
+    windowSamples = cumulantsElsewhere(order:maxLag, order); 
+    
+    % Assign the window samples to the appropriate locations in the corresponding cumulant component matrices
+    cumulantComponent32(maxLag+1-order, 1:maxLag+1-order) = windowSamples'; 
+    cumulantComponent34(1:maxLag+1-order, maxLag+1-order) = windowSamples; 
+    
+    % If the order is less than maxLag, update cumulantComponent33 by considering diagonals
+    if (order < maxLag) 
+        windowSamples = flipud(windowSamples(2:length(windowSamples))); 
+        cumulantComponent33 = cumulantComponent33 + diag(windowSamples, order) + diag(windowSamples, -order); 
     end 
 end 
 
-c33  = c33 + diag(c3(1,nlag+1:-1:2)); 
-cmat = [ [c33, c32, zeros(nlag,1)]; [ [c34; zeros(1,nlag)] , c3 ] ]; 
+% Add the main diagonal elements from the first row of symmetricCumulantsMatrix to cumulantComponent33
+cumulantComponent33  = cumulantComponent33 + diag(cumulantsMatrix(1, maxLag+1:-1:2)); 
+
+% Construct the final cumulants matrix by combining cumulantComponent33, cumulantComponent32, and cumulantComponent34
+cmat = [ [cumulantComponent33, cumulantComponent32, zeros(maxLag, 1)]; [ [cumulantComponent34; zeros(1, maxLag)] , cumulantsMatrix ] ]; 
       
 %% Apply lag-domain window
-wcmat = cmat; 
-if (wind ~= 1) 
-     indx = (-nlag:nlag)';
-     for k=-nlag:nlag
-         wcmat(:,k+nlag+1) = cmat(:,k+nlag+1).* window(abs(indx-k)+1) .* window(abs(indx)+1)* window(abs(k)+1); 
+
+% Initialize the windowed cumulants matrix
+windowedCumulantsMatrix = cumulantsMatrix; 
+
+% Check if the window type is not equal to 1 (indicating no windowing)
+if (windowType ~= 1)
+    % Create an index vector for the lag values
+     lagIndices = (-maxLag:maxLag)';
+
+     % Loop over each lag value
+     for recordIndex=-maxLag:maxLag
+         % Apply the lag-domain window to each column of the cumulants matrix
+         windowedCumulantsMatrix(:,recordIndex+maxLag+1) = cumulantsMatrix(:,recordIndex+maxLag+1).* window(abs(lagIndices-recordIndex)+1) .* window(abs(lagIndices)+1)* window(abs(recordIndex)+1); 
      end 
 end
 
-%% Compute 2d-fft, shift and rotate for proper orientation
-Bspec = fft2(wcmat, nfft, nfft); 
-Bspec = fftshift(Bspec);               % axes d and r; orig at ctr
+%% Compute 2D-FFT, shift, and rotate for proper orientation
 
-if (rem(nfft,2) == 0) 
-    waxis = (-nfft/2:(nfft/2-1))/nfft;
+% Compute the 2D-FFT of the windowed cumulants matrix
+bispectrum = fft2(windowedCumulantsMatrix, fftLength, fftLength); 
+
+% Shift the bispectrum to center the axes d and r
+bispectrum = fftshift(bispectrum);
+
+% Compute the frequency axis values
+if (rem(fftLength,2) == 0)
+    % For even fftLength, use the appropriate frequency axis values
+    waxis = (-fftLength/2:(fftLength/2-1))/fftLength;
 else
-    waxis = (-(nfft-1)/2:(nfft-1)/2)/nfft;
+    % For odd fftLength, use the appropriate frequency axis values
+    waxis = (-(fftLength-1)/2:(fftLength-1)/2)/fftLength;
 end 
 
-contour(waxis,waxis,abs(Bspec),4), grid on 
+% Plot the magnitude of the bispectrum using contour plot
+contour(waxis,waxis,abs(bispectrum),4), grid on
+
+% Set the title and labels for the plot
 title('Bispectrum estimated via the indirect method - Rectangular window')
 xlabel('f1'), ylabel('f2')
 end
